91b4f7c867ec174d69938da17077f1bd0cb2d9de
Yogesh Upadhyay <yogeshupadhya@gmail.com>
2016-10-14 00:51:05 +0100

    few more addition

--- app/app.py
+++ app/app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request, json
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-	return 'Hello World'
+def index():
+	return render_template('index.html')
 
 @app.route('/home')
 def home():
-    return "Home"
+    return render_template('home.html')
 
+if __name__=="__main__":
+    app.run()
